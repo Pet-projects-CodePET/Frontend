71caf8f6e08a2ae516a37a8b280e55b1a5ff90bd
jsapro <77.3.77@mail.ru>
2024-02-02 21:43:33 +0300

    add variativity for button through props

--- src/components/shared/ui/main-button/index.tsx
+++ src/components/shared/ui/main-button/index.tsx
@@ -1,17 +1,55 @@
 import { FC } from 'react';
-import LikeIcon from '@/components/shared/ui/icons/like-icon.svg';
-import ChevronRightIcon from '@/components/shared/ui/icons/chevron-right-icon.svg';
 import type { MainButtonProps } from './types';
 import styles from './main-button.module.scss';
 
-const MainButton: FC<MainButtonProps> = ({ children, ...props }) => {
+const MainButton: FC<MainButtonProps> = ({
+	children,
+	IconLeft,
+	IconRight,
+	variant,
+	width,
+	...props
+}) => {
+	const getClassnameForType = (
+		buttonType: 'primary' | 'secondary' | 'trivial' | 'inverse'
+	) => {
+		switch (buttonType) {
+			case 'primary':
+				return styles.buttonPrimary;
+			case 'secondary':
+				return styles.buttonSecondary;
+			case 'trivial':
+				return styles.buttonTrivial;
+			case 'inverse':
+				return styles.buttonInverse;
+			default:
+				return '';
+		}
+	};
+
+	const getClassNameForWidth = (buttonWidth: 'regular' | 'max') => {
+		switch (buttonWidth) {
+			case 'regular':
+				return styles.buttonDimensionsRegular;
+			case 'max':
+				return styles.buttonDimensionsMax;
+			default:
+				return '';
+		}
+	};
+
 	return (
 		<>
-			<button className={styles.button} {...props}>
-				{children}
+			<button
+				className={`${getClassnameForType(variant)} ${getClassNameForWidth(width)}`}
+				{...props}
+			>
+				<div className={styles.buttonContainer}>
+					{IconLeft && <IconLeft className={styles.icon} />}
+					{children}
+					{IconRight && <IconRight className={styles.icon} />}
+				</div>
 			</button>
-			<LikeIcon className={styles.icon} />
-			<ChevronRightIcon className={styles.icon} />
 		</>
 	);
 };
--- src/components/shared/ui/main-button/types.ts
+++ src/components/shared/ui/main-button/types.ts
@@ -2,4 +2,8 @@ import { ButtonHTMLAttributes, ReactNode } from 'react';
 
 export type MainButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
 	children: ReactNode;
+	variant: 'primary' | 'secondary' | 'trivial' | 'inverse';
+	width: 'regular' | 'max';
+	IconLeft?: React.ComponentType<React.HTMLAttributes<HTMLElement>>;
+	IconRight?: React.ComponentType<React.HTMLAttributes<HTMLElement>>;
 };
