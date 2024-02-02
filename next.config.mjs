edd956b5c967991c8bb4e11d97573fbf46e95f20
jsapro <77.3.77@mail.ru>
2024-02-02 18:45:30 +0300

    change next.js config to work with @svgr/webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: [{ loader: '@svgr/webpack', options: { icon: true } }],
+		});
+		return config;
+	},
+};
 
 export default nextConfig;
