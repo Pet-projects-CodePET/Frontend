4ab137b310b0796c4b593cb8ba96430141367cc5
makc-anisimov <makc.anisimov@gmail.com>
2024-02-08 01:28:21 +0300

    [+]add iconbutton component tsx and styles

---
+++ src/shared/ui/iconbutton/iconbutton.tsx
@@ -0,0 +1,43 @@
+import { FC } from 'react';
+
+import type { IconButtonProps } from './types';
+import styles from './iconbutton.module.scss';
+
+export const IconButton: FC<IconButtonProps> = ({
+	variant,
+	size
+}) => {
+	const getClassnameForType = (
+		buttonType: 'gmail' | 'vk' | 'yandex' | 'git'
+	) => {
+		switch (buttonType) {
+			case 'gmail':
+				return styles.buttonGmail;
+			case 'vk':
+				return styles.buttonVk;
+			case 'yandex':
+				return styles.buttonYandex;
+			case 'git':
+				return styles.buttonGit;
+			default:
+				return '';
+		}
+	};
+	const getClassNameForSize = (buttonSize: 'desktop' | 'mobile') => {
+		switch (buttonSize) {
+			case 'desktop':
+				return styles.buttonSizeDesktop;
+			case 'mobile':
+				return styles.buttonSizeMobile;
+			default:
+				return '';
+		}
+	};
+
+	return (
+		<button
+			className={`${getClassnameForType(variant)} ${getClassNameForSize(size)}`}
+		/>
+		// </button>
+	);
+};
