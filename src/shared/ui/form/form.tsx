cd02f8d5c01ba2bb2a9ed28e12120198770c78e5
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-02-08 21:41:02 +0300

    feat(form): add Form component

--- src/shared/ui/form/form.tsx
+++ src/shared/ui/form/form.tsx
@@ -2,44 +2,38 @@
 
 import React, { FC } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
 import styles from './form.module.scss';
 
 interface FormProps extends React.HTMLAttributes<HTMLFormElement> {
 	extraClass?: string;
-	isUpdating?: boolean;
-	submitText?: string;
-	title?: string;
-	subtitle?: string;
 	schema?: yup.AnyObjectSchema;
 	onSubmit: (data: Record<string, any>) => void;
 }
 
 const Form: FC<FormProps> = ({
 	extraClass,
-	submitText,
-	title,
-	subtitle,
-	isUpdating = false,
 	children,
 	schema,
 	onSubmit,
 	...FormHTMLAttributes
 }) => {
-	const methods = useForm();
+	const methods = useForm({
+		resolver: schema ? yupResolver(schema) : undefined,
+	});
+
+	const { handleSubmit } = methods;
 
 	return (
 		<FormProvider {...methods}>
 			<form
 				className={`${styles.form} ${extraClass}`}
-				onSubmit={methods.handleSubmit(onSubmit)}
+				onSubmit={handleSubmit(onSubmit)}
 				{...FormHTMLAttributes}
 			>
-				<h1 className={styles.title}>{title}</h1>
-				<h2 className={styles.subtitle}>{subtitle}</h2>
 				{children}
-				<button type="submit">Отправить данные</button>
 			</form>
 		</FormProvider>
 	);
