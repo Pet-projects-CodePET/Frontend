86178b594efdce616e0b5e7ad7ddd51fcce3e7d3
jsapro <77.3.77@mail.ru>
2024-02-10 12:42:58 +0300

    feat: add Promo component

---
+++ src/widgets/navBar/ui/promo/promo.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import Image from 'next/image';
+
+import { MainButton } from '@/shared/ui';
+import backgroundImage from '@/shared/assets/images/backgroundLoginLayout.png';
+
+import styles from './promo.module.scss';
+
+export const Promo = () => {
+	return (
+		<section className={styles.promo__container}>
+			<div className={styles.promo__imageContainer}>
+				<Image
+					src={backgroundImage}
+					alt="abstract image"
+					className={styles.promo__image}
+					priority={true}
+					fill
+				></Image>
+			</div>
+			<div className={styles.promo__absoluteContainer}>
+				<div className={styles.promo__textContainer}>
+					<p className={styles.promo__title}>
+						Пет-проекты для всех и идеальный для тебя
+					</p>
+					<p className={styles.promo__subtitle}>
+						CodePET - платформа, которая дает возможности для сотрудничества,
+						обмена знаниями и портфолио
+					</p>
+				</div>
+				<div className={styles.promo__squareContainer}>
+					<div className={styles.promo__square}>
+						<p className={styles.promo__squareText}>1714</p>
+						<p className={styles.promo__squareText2}>проектов</p>
+					</div>
+					<div className={styles.promo__square}>
+						<p className={styles.promo__squareText}>1714</p>
+						<p className={styles.promo__squareText2}>участников</p>
+					</div>
+				</div>
+				<div className={styles.promo__button}>
+					<MainButton onClick={console.log} variant="primary" width="max">
+						Создать проект
+					</MainButton>
+				</div>
+			</div>
+		</section>
+	);
+};
