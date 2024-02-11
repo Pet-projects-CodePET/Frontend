94f35104541eb6dbe65004b822048b3791295daf
jsapro <77.3.77@mail.ru>
2024-02-11 18:51:38 +0300

    refactor: (after rebase) delete unused code

---
+++ src/features/authentication/ui/loginForm.tsx
@@ -0,0 +1,9 @@
+import { loginThunk } from '../model/login';
+import { LoginSchema } from '../model/loginShema';
+import cls from './loginForm.module.scss';
+
+export const LoginForm = () => {
+    return (
+        <div></div>
+    )
+}
