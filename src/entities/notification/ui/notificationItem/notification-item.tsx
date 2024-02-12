1469f5db53469e40dc90c62c870b0a5a84af04da
Liolik <aleksey.taratuta@gmail.com>
2024-02-12 23:04:41 +0300

    lint fix

--- src/entities/notification/ui/notificationItem/notification-item.tsx
+++ src/entities/notification/ui/notificationItem/notification-item.tsx
@@ -1,5 +1,1 @@
-import { Notification } from '../../model/notification';
-import cls from './NotificationItem.module.scss';
-import { getQueryParams } from '@/shared/lib';
-
 export const NotificationItem = () => {};
