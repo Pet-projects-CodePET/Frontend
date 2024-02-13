eb46b7176d81e68e1dd5a414fbc5e6347ba1ac50
unknown <panterajuliapapina@yandex.ru>
2024-02-13 13:46:57 +0300

    feat: header screen pad, mobile

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -7,7 +7,9 @@ import { MainButton } from '@/shared/ui';
 import { NavBar } from '@/widgets/nav-bar';
 import { navBarLinksArray } from '@/shared/constants';
 import IconUser from '@/shared/assets/icons/icon-user.svg';
-import LogoIcon from '../../../shared/assets/images/logo-header.svg';
+import LogoIcon from '@/shared/assets/images/logo-header.svg';
+import MenuBurger from '@/shared/assets/icons/dots-vertical.svg';
+import IconPlus from '@/shared/assets/icons/plus-large.svg';
 import styles from './header.module.scss';
 
 export const Header = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
@@ -17,36 +19,46 @@ export const Header = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
 			<div className={styles.header__container}>
 				<div className={styles.header__wrapper}>
 					<Link href="/" className={styles.header__link}>
-						<Image src={LogoIcon} alt="logo" />
+						<Image src={LogoIcon} alt="logo" className={styles.header__logo} />
 					</Link>
 
-					<NavBar navBarLinksArray={navBarLinksArray}/>
-					{isLoggedIn ? (
-						<MainButton
-							variant="inverse"
-							width="max"
-							onClick={() => router.push('new-project')}
-						>
-							&#43;  Создать проект
-						</MainButton>
+					<NavBar navBarLinksArray={navBarLinksArray} />
+
+					{!isLoggedIn ? (
+						<div className={styles.header__buttonProject}>
+							<MainButton
+								variant="inverse"
+								width="max"
+								onClick={() => router.push('new-project')}
+							>
+								<Image src={IconPlus} alt="icon plus" />
+								Создать проект
+							</MainButton>
+						</div>
 					) : (
-						<MainButton
-							variant="inverse"
-							width="max"
-							onClick={() => router.push('login')}
-						>
-							Войти
-						</MainButton>
+						<div className={styles.header__buttonLogin}>
+							<MainButton
+								variant="inverse"
+								width="max"
+								onClick={() => router.push('login')}
+							>
+								Войти 
+							</MainButton>
+						</div>
 					)}
+
+					<button className={styles.header__buttonBurger}>
+						<Image src={MenuBurger} alt="Menu burger" />
+					</button>
 				</div>
 				<div>
-					{isLoggedIn && (
-						<Link href="profile">
+					{!isLoggedIn && (
+						<Link href="profile" className={styles.header__linkProfile}>
 							<Image src={IconUser} alt="iconUser" />
 						</Link>
 					)}
 				</div>
-			</div>		
+			</div>
 		</div>
 	);
 };
