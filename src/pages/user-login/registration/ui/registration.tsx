4ef7d6d7934b814cbbc3397a29d47590f978a77c
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-02-14 23:01:19 +0300

    feat(form-signup): add form-signup component

--- src/pages/user-login/registration/ui/registration.tsx
+++ src/pages/user-login/registration/ui/registration.tsx
@@ -1,15 +1,13 @@
 'use client';
 
 import React from 'react';
+import { FormSignup } from '@/widgets/form-signup/ui/form-signup';
 import styles from './signup.module.scss';
-import Form from '@/shared/ui/form/form';
 
 export const Signup = () => {
-	const handleSubmit = () => console.log('Форма регистрации отправляет запрос');
-
 	return (
 		<div className={styles.container}>
-			<Form onSubmit={handleSubmit}>Форма регистрации</Form>
+			<FormSignup />
 		</div>
 	);
-};
+}
