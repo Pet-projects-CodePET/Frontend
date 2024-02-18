945d2c83f4be93fcb9246523105c12ca5309b532
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-02-18 08:47:26 +0300

    feat(form-password-recovery): add form password-precovery

---
+++ src/features/ui/form-password-recovery/form-password-recovery.tsx
@@ -0,0 +1,114 @@
+'use client';
+
+import { IconButtonList } from '@/entities/iconButtonList';
+import { Form, Input, MainButton } from '@/shared/ui';
+import HCaptcha from '@hcaptcha/react-hcaptcha';
+import Link from 'next/link';
+
+import React, { FC, useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import styles from './form-password-recovery.module.scss';
+
+export const FormPasswordRecovery: FC = ({}) => {
+	const [isPasswordSend, setIsPasswordSend] = useState(false);
+	const { register } = useForm();
+
+	const captchaRef = useRef<HCaptcha>(null);
+	const sitekey: string = process.env.NEXT_PUBLIC_CAPTCHA_SITE_KEY || '';
+
+	console.log(sitekey);
+
+	const onLoad = () => {
+		const executePayload = { async: true };
+		captchaRef.current?.execute(executePayload);
+	};
+
+	const setToken = (token: string) => {
+		console.log('Капча пройдена. Токен:', token);
+	};
+
+	const handlePasswordSend = () => {
+		setIsPasswordSend(true);
+	};
+
+	const handlePasswordReSend = () => {
+		setIsPasswordSend(false);
+	};
+
+	const handleSubmit = (data: unknown) => {
+		handlePasswordSend();
+		console.log(data);
+	};
+
+	return (
+		<Form onSubmit={handleSubmit}>
+			<h1 className={styles.title}>Восстановление пароля</h1>
+			{isPasswordSend && (
+				<p className={styles.title_message}>
+					На ваш e-mail был отправлен новый пароль
+				</p>
+			)}
+			<div className={styles.container}>
+				<div className={styles.input_list}>
+					<Input
+						label="email"
+						labelName="E-mail"
+						placeholder="Введите e-mail"
+						register={register}
+						error={'Так выглядит ошибка'}
+					/>
+					{isPasswordSend && (
+						<Input
+							label="password"
+							labelName="Пароль"
+							type={'password'}
+							register={register}
+							placeholder="Введите пароль"
+						/>
+					)}
+				</div>
+				{!isPasswordSend && (
+					<HCaptcha
+						sitekey={sitekey}
+						onVerify={setToken}
+						onLoad={onLoad}
+						ref={captchaRef}
+					/>
+				)}
+				<MainButton variant={'primary'} width={'max'}>
+					{!isPasswordSend ? 'Восстановить пароль' : 'Войти'}
+				</MainButton>
+			</div>
+			{!isPasswordSend ? (
+				<>
+					<div className={styles.container}>
+						<span className={styles.iconsButtons_line}>или</span>
+						<IconButtonList />
+					</div>
+					<div className={styles.container}>
+						<p className={styles.text_registration}>
+							Нет аккаунта?&#160;
+							<Link className={styles.link_registration} href={'/registration'}>
+								Зарегистрироваться
+							</Link>
+						</p>
+						<p className={styles.text_policy}>
+							При создании аккаунта вы соглашаетесь с&#160;
+							<Link className={styles.link_policy} href={'/login'}>
+								политикой&#160;об&#160;использовании персональных данных
+							</Link>
+						</p>
+					</div>
+				</>
+			) : (
+				<Link
+					className={styles.link_registration}
+					href={'/login/password-recovery'}
+					onClick={handlePasswordReSend}
+				>
+					Отправить пароль повторно
+				</Link>
+			)}
+		</Form>
+	);
+};
