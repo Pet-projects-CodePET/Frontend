5d6a4703832f0b6f3e7a9b32b548529edfeaa5dd
unknown <panterajuliapapina@yandex.ru>
2024-02-19 18:14:52 +0300

    feat(widgets): added onclick for button registration in joinUs

--- src/widgets/join-us/ui/join-us.tsx
+++ src/widgets/join-us/ui/join-us.tsx
@@ -1,15 +1,17 @@
 'use client';
 
 import React from 'react';
+import { useRouter } from 'next/navigation';
 import { MainButton } from '@/shared/ui';
 import styles from './join-us.module.scss'
 
 export const JoinUs = () => {
+  const router = useRouter();
   return (
-    <div className={styles.joinus}>
+    <div className={styles.joinus} >
       <h2 className={styles.joinus__title}>Присоединяйся к нам!</h2>
       <p className={styles.joinus__text}>Создай свой проект, собери команду и&nbsp;стань на&nbsp;шаг ближе к&nbsp;мечте</p>
-      <MainButton variant="primary" width="regular">
+      <MainButton variant="primary" width="regular" onClick={() => router.push('registration')}>
         Зарегистрироваться
       </MainButton>
     </div>
