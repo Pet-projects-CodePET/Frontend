c40c34f7ffa6fb0750aad91417aabcfce9b68539
unknown <panterajuliapapina@yandex.ru>
2024-02-20 20:24:52 +0300

    feat(entities): moved side nav to entities, change styles, removed rout new-project

--- src/pages/new-project-page/ui/new-project-page.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const NewProject = () => {
-	return <div>Новый проект</div>;
-};
--- src/shared/layouts/profile-layout/profile-layout.tsx
+++ src/shared/layouts/profile-layout/profile-layout.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
-import styles from './profile-layout.module.scss';
 import { Header } from '@/widgets/header';
 import { Footer } from '@/widgets/footer';
-import { SideNav } from '@/widgets/side-nav';
+import { SideNav } from '@/entities/side-nav';
+import styles from './profile-layout.module.scss';
 
 export const ProfileLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
-        <>
-		<div className={styles.wrapper}>
-			<Header isLoggedIn />
-        <div className={styles.wrapper__title}>
-		<h2 className={styles.wrapper__titleText}>Личный кабинет</h2>
-		</div>
-            
-			<div className={styles.container}>
-            <SideNav/>
-			<div>{children}</div>
+		<>
+			<div className={styles.wrapper}>
+				<Header isLoggedIn />
+				<div className={styles.wrapper__title}>
+					<h2 className={styles.wrapper__titleText}>Личный кабинет</h2>
+				</div>
 
-            </div>
-
-			
-		</div>
-        <Footer />
-        </>
+				<div className={styles.container}>
+					<SideNav />
+					<div>{children}</div>
+				</div>
+			</div>
+			<Footer />
+		</>
 	);
 };
