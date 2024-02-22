50e9e754c3de53feefeb0122c86f0e082b8f2c97
jsapro <77.3.77@mail.ru>
2024-02-22 12:06:48 +0300

    fix(app): linter autofix

--- src/widgets/join-us/ui/join-us.tsx
+++ src/widgets/join-us/ui/join-us.tsx
@@ -7,15 +7,20 @@ import { MainButton } from '@/shared/ui';
 import styles from './join-us.module.scss';
 
 export const JoinUs = () => {
-  const router = useRouter();
-  return (
-    <div className={styles.joinus} >
-      <h2 className={styles.joinus__title}>Присоединяйся к нам!</h2>
-      <p className={styles.joinus__text}>Создай свой проект, собери команду и&nbsp;стань на&nbsp;шаг ближе к&nbsp;мечте</p>
-      <MainButton variant="primary" width="regular" onClick={() => router.push('registration')}>
-        Зарегистрироваться
-      </MainButton>
-    </div>
-
-  )
-}
+	const router = useRouter();
+	return (
+		<div className={styles.joinus}>
+			<h2 className={styles.joinus__title}>Присоединяйся к нам!</h2>
+			<p className={styles.joinus__text}>
+				Создай свой проект, собери команду и&nbsp;стань на&nbsp;шаг ближе
+				к&nbsp;мечте
+			</p>
+			<MainButton
+				variant="primary"
+				width="regular"
+				onClick={() => router.push('registration')}>
+				Зарегистрироваться
+			</MainButton>
+		</div>
+	);
+};
