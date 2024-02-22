f41d4debb1357d0d21133613c24ac0b7443ccc0f
jsapro <77.3.77@mail.ru>
2024-02-22 13:34:14 +0300

    fix(shared): improve types and fix errors

--- src/shared/ui/tags/types.ts
+++ src/shared/ui/tags/types.ts
@@ -1,9 +1,11 @@
 import { HTMLAttributes } from 'react';
 
+export type Tag = {
+	id: number;
+	text: string;
+	color: string;
+};
+
 export type TagsProps = HTMLAttributes<HTMLElement> & {
-	tags: {
-		id: number;
-		text: string;
-		color: string;
-	}[];
+	tags: Tag[];
 };
