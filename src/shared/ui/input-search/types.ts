9090afc3f6683e7ece885e72b171c80b32b582c0
Vasiliy Konovalov <vvkon13@gmail.com>
2024-02-24 22:49:05 +0400

    feat(shared): add input search double
    МЕТА ДАННЫЕ: #42

--- src/shared/ui/input-search/types.ts
+++ src/shared/ui/input-search/types.ts
@@ -1,6 +1,6 @@
 import { InputHTMLAttributes } from 'react';
 
 export type InputSearchProps = InputHTMLAttributes<HTMLInputElement> & {
-    search: (str:string)=>void;
-    delay?:number;
+	search: (str: string) => void;
+	delay?: number;
 };
