eac9d54c26e421e14aa69929ff7b837806a13a37
unknown <panterajuliapapina@yandex.ru>
2024-02-28 21:30:25 +0300

    feat(shared): added type checkbox, checkbox variant, styles checkbox

--- src/entities/profile-settings/ui/profile-settings.tsx
+++ src/entities/profile-settings/ui/profile-settings.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { MainButton } from '@/shared/ui';
 import { Input } from '@/shared/ui';
 import { Form } from '@/shared/ui';
@@ -12,6 +12,12 @@ import { ToggleCheckbox } from '@/shared/ui/toggle-checkbox/toggle-checkbox';
 
 export const ProfileSettings = () => {
 	const { register } = useForm();
+	const [checked, setChecked] = useState(false);
+
+	const checkedChange = (checked: boolean) => {
+		setChecked(checked);
+		console.log('выполнить действие чекбокса');
+	};
 
 	const handleSubmit = () => {
 		console.log('Submit');
@@ -26,24 +32,139 @@ export const ProfileSettings = () => {
 						<IconDown className={styles.formSettings__icon} />
 					</div>
 
+					<div className={styles.formSettings__itemVisible}>
+						<label
+							className={styles.formSettings__subtitle}
+							htmlFor="visibleEveryone">
+							Видно всем
+						</label>
+						<div className={styles.formSettings__checkbox}>
+							<ToggleCheckbox
+								id="visibleEveryone"
+								name="visibleEveryone"
+								variant="defaultOf"
+								onChange={() => {}}
+							/>
+						</div>
+					</div>
+					<div className={styles.formSettings__itemVisible}>
+						<label
+							className={styles.formSettings__subtitle}
+							htmlFor="visibleOrganizers">
+							Видно организаторам
+						</label>
+						<div className={styles.formSettings__checkbox}>
+							<ToggleCheckbox
+								id="visibleOrganizers"
+								name="visibleOrganizers"
+								variant="defaultOf"
+								onChange={() => {}}
+							/>
+						</div>
+					</div>
+
+					<div className={styles.formSettings__itemVisible}>
+						<label
+							className={styles.formSettings__subtitle}
+							htmlFor="noVisible">
+							Не видно никому
+						</label>
+						<div className={styles.formSettings__checkbox}>
+							<ToggleCheckbox
+								id="noVsible"
+								name="noVisible"
+								variant="defaultOf"
+								onChange={() => {
+									console.log('не видно');
+								}}
+							/>
+						</div>
+					</div>
+
 					<div className={styles.formSettings__item}>
 						<p className={styles.formSettings__subtitle}>Видимость контактов</p>
 						<IconDown className={styles.formSettings__icon} />
 					</div>
 
-					<div className={styles.formSettings__item}>
-						<p className={styles.formSettings__subtitle}>
-							Отправка уведомлений
-						</p>
+					<div className={styles.formSettings__itemVisible}>
+						<label
+							className={styles.formSettings__subtitle}
+							htmlFor="visible-everyone">
+							Видно всем
+						</label>
 						<div className={styles.formSettings__checkbox}>
-							<ToggleCheckbox />
+							<ToggleCheckbox
+								id="visible-everyone"
+								name="visible-everyone"
+								variant="defaultOf"
+								onChange={() => {}}
+							/>
+						</div>
+					</div>
+					<div className={styles.formSettings__itemVisible}>
+						<label
+							className={styles.formSettings__subtitle}
+							htmlFor="visible-organizers">
+							Видно организаторам
+						</label>
+						<div className={styles.formSettings__checkbox}>
+							<ToggleCheckbox
+								id="visible-organizers"
+								name="visible-organizers"
+								variant="defaultOf"
+								onChange={() => {}}
+							/>
+						</div>
+					</div>
+
+					<div className={styles.formSettings__itemVisible}>
+						<label
+							className={styles.formSettings__subtitle}
+							htmlFor="no-visible">
+							Не видно никому
+						</label>
+						<div className={styles.formSettings__checkbox}>
+							<ToggleCheckbox
+								id="no-visible"
+								name="no-visible"
+								variant="defaultOf"
+								onChange={() => {
+									console.log('не видно');
+								}}
+							/>
 						</div>
 					</div>
 
 					<div className={styles.formSettings__item}>
-						<p className={styles.formSettings__subtitle}>Подписка на проекты</p>
+						<label className={styles.formSettings__subtitle} htmlFor="notify">
+							Отправка уведомлений
+						</label>
 						<div className={styles.formSettings__checkbox}>
-							<ToggleCheckbox />
+							<ToggleCheckbox
+								id="notify"
+								name="notify"
+								variant="defaultOn"
+								checked={checked}
+								onChange={checkedChange}
+							/>
+						</div>
+					</div>
+
+					<div className={styles.formSettings__item}>
+						<label
+							className={styles.formSettings__subtitle}
+							htmlFor="subscription">
+							Подписка на проекты
+						</label>
+						<div className={styles.formSettings__checkbox}>
+							<ToggleCheckbox
+								variant="defaultOn"
+								id="subscription"
+								name="subscription"
+								onChange={() => {
+									console.log('подписка');
+								}}
+							/>
 						</div>
 					</div>
 				</div>
@@ -55,30 +176,30 @@ export const ProfileSettings = () => {
 			<Form onSubmit={handleSubmit} className={styles.formSettings}>
 				<h2 className={styles.formSettings__title}>Смена пароля</h2>
 				<div className={styles.formSettings__listPassword}>
-				<Input
-				    className={styles.formSettings__input}
-					label="password"
-					type="password"
-					labelName="Старый пароль"
-					register={register}
-					description
-				/>
-				<Input
-				className={styles.formSettings__input}
-					label="new-password"
-					type="password"
-					labelName="Новый пароль"
-					register={register}
-					description
-				/>
-				<Input
-				className={styles.formSettings__input}
-					label="repeat-new-password"
-					type="password"
-					labelName="Новый пароль еще раз"
-					register={register}
-					description
-				/>
+					<Input
+						className={styles.formSettings__input}
+						label="password"
+						type="password"
+						labelName="Старый пароль"
+						register={register}
+						description
+					/>
+					<Input
+						className={styles.formSettings__input}
+						label="new-password"
+						type="password"
+						labelName="Новый пароль"
+						register={register}
+						description
+					/>
+					<Input
+						className={styles.formSettings__input}
+						label="repeat-new-password"
+						type="password"
+						labelName="Новый пароль еще раз"
+						register={register}
+						description
+					/>
 				</div>
 
 				<div>
--- src/shared/ui/toggle-checkbox/toggle-checkbox.tsx
+++ src/shared/ui/toggle-checkbox/toggle-checkbox.tsx
@@ -1,14 +1,59 @@
 import React from 'react';
-import styles from './toggle-checkbox.module.scss';
+// import { clsx } from 'clsx';
 
-export const ToggleCheckbox = () => {
+import styles from './toggle-checkbox.module.scss';
+type ToggleCheck = {
+	id: string;
+	name: string;
+	variant:  'defaultOn' | 'defaultOf' | 'disabledOn' | 'disabledOf' | 'errorDefaultOn' | 'errorDefaultOF',
+	checked?: boolean;
+	onChange: (checked: boolean) => void;
+	disabled?: boolean;
+};
+
+export const ToggleCheckbox = ({
+	id,
+	name,
+	variant,
+	onChange,
+	checked,
+	disabled,
+}: ToggleCheck) => {
+
+	const getClassnameForType = (
+		toggleCheckboxType: 'defaultOn' | 'defaultOf' | 'disabledOn' | 'disabledOf' | 'errorDefaultOn' | 'errorDefaultOF'
+	) => {
+		switch (toggleCheckboxType) {
+			case 'defaultOn':
+				return styles.toggleCheckbox__defaultOn;
+			case 'defaultOf':
+				return styles.toggleCheckbox__defaultOf;
+			case 'disabledOn':
+				return styles.toggleCheckbox__disabledOn;
+			case 'disabledOf':
+				return styles.toggleCheckbox__disabledOf;
+		   case 'errorDefaultOn':
+				return styles.toggleCheckbox__errorDefaultOn;
+		  case 'errorDefaultOF':
+				return styles.toggleCheckbox__errorDefaultOf;
+				default:
+				return '';	
+		}
+	};
+
 	return (
-		<label className={styles.toggleCheckbox}>
+		<label className={styles.toggleCheckbox} htmlFor={name}>
 			<input
 				className={styles.toggleCheckbox__input}
 				type="checkbox"
+				id={id}
+				name={name}
+				checked={checked}
+				onChange={(e) => onChange(e.target.checked)}
+				disabled={disabled}
 			/>
-			<span className={styles.toggleCheckbox__text}></span>
+			<span
+				className={getClassnameForType(variant)}></span>
 		</label>
 	);
 };
