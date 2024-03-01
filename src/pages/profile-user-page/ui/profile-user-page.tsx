4284d5ab352faee2fb4fe6087b4afc9191c1b29f
Aleksej Taratuta <116726052+Liolikmolo@users.noreply.github.com>
2024-03-01 15:57:42 +0300

    добавил закрытие по крестику, клавишей Esc и кликом на подложку

--- src/pages/profile-user-page/ui/profile-user-page.tsx
+++ src/pages/profile-user-page/ui/profile-user-page.tsx
@@ -1,12 +1,19 @@
+'use client';
+
 import React from 'react';
+import { useState } from 'react';
 import { ProfileEditForm } from '@/entities/profile-edit-form';
 import { PopUp } from '@/shared/ui/pop-up/pop-up';
 
 export const ProfileUser = () => {
+	const [isPopup, setIsPopup] = useState(false);
+	const onClose = () => setIsPopup(false);
+
 	return (
 		<>
 			<div>Личный кабинет</div>
-			<PopUp>
+			<button onClick={() => setIsPopup(true)}>Show PopUp</button>
+			<PopUp visible={isPopup} title="Popup" onClose={onClose}>
 				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore
 				aspernatur possimus alias magni molestias aut laudantium aperiam sint
 				corporis! Magnam accusamus provident reprehenderit sunt nam quas, maxime
--- src/shared/ui/pop-up/pop-up.tsx
+++ src/shared/ui/pop-up/pop-up.tsx
@@ -1,17 +1,45 @@
-import React, { ReactNode } from 'react';
+'use client';
+
+import React, { ReactNode, useEffect } from 'react';
 import style from './pop-up.module.scss';
 import XmarkIcon from '@/shared/assets/icons/xmark.svg';
 
 type PopUpProps = {
 	children: ReactNode;
+	visible: boolean;
+	title: string;
+	onClose: () => void;
 };
 
-export const PopUp = ({ children }: PopUpProps) => {
+export const PopUp = ({
+	children,
+	visible = false,
+	title,
+	onClose,
+}: PopUpProps) => {
+	const onKeyDown = ({ key }: KeyboardEvent) => {
+		switch (key) {
+			case 'Escape':
+				onClose();
+				break;
+			default:
+				return null;
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener('keydown', onKeyDown);
+		return () => document.removeEventListener('keydown', onKeyDown);
+	});
+
+	if (!visible) return null;
+
 	return (
-		<div className={style.body}>
-			<div className={style.wrapper}>
-				<div className={style.content}>
-					<XmarkIcon className={style.xmark} />
+		<div className={style.popup_body} onClick={onClose}>
+			<div className={style.popup_wrapper}>
+				<div className={style.popup_content}>
+					<div className={style.popup_title}>{title}</div>
+					<XmarkIcon className={style.xmark} data-close onClick={onClose} />
 					{children}
 				</div>
 			</div>
