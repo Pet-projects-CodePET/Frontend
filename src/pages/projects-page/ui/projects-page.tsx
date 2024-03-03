afb28c1350696e39d3b6722898464b5923eb91ca
DBKai <dbkai@ya.ru>
2024-03-03 14:43:20 +0500

    feat(pages): changed projects data to ai generated list

--- src/pages/projects-page/ui/projects-page.tsx
+++ src/pages/projects-page/ui/projects-page.tsx
@@ -4,70 +4,17 @@ import React from 'react';
 import { ProjectCardFull } from '@/widgets/project-card-full';
 import styles from './projects-page.module.scss';
 import { SingleSelect } from '@/widgets/single-select/ui/single-select';
-import { statusOptions } from '@/widgets/single-select/type';
-
-// TODO удалить projects когда будут данные с сервера
-const projects = [
-	{
-		id: 1,
-		professions: [
-			'UX/UI',
-			'Manual QA',
-			'PM',
-			'System Analyst',
-			'Front-end',
-			'DevOps',
-		],
-		skills: ['Навык1', 'Навык2', 'Навык3'],
-		description: ` Lorem ipsum`.repeat(15),
-		isActiveProject: true,
-		duration: '15 сентября-22 августа',
-		title: 'Название проекта',
-		subtitle: 'Мобильная разработка',
-	},
-	{
-		id: 2,
-		professions: [
-			'UX/UI',
-			'Manual QA',
-			'PM',
-			'System Analyst',
-			'Front-end',
-			'DevOps',
-		],
-		skills: ['Навык1', 'Навык2', 'Навык3'],
-		description: ` Lorem ipsum`.repeat(15),
-		isActiveProject: false,
-		duration: '10 сентября-15 августа',
-		title: 'Название проекта2',
-		subtitle: 'Мобильная разработка',
-	},
-	{
-		id: 3,
-		professions: [
-			'UX/UI',
-			'Manual QA',
-			'PM',
-			'System Analyst',
-			'Front-end',
-			'DevOps',
-		],
-		skills: ['Навык1', 'Навык2', 'Навык3'],
-		description: ` Lorem ipsum`.repeat(15),
-		isActiveProject: true,
-		duration: '15 сентября-22 августа',
-		title: 'Название проекта3',
-		subtitle: 'Мобильная разработка',
-	},
-];
+import { statusOptions } from '@/shared/types/status-options';
+import { projects } from '@/shared/types/projects';
 
 export const Projects = () => {
 	return (
 		<>
 			<div className={styles.filterContainer}>
 				<SingleSelect
+					name="select-status"
 					options={statusOptions}
-					selectedOption={{ value: 'completed', label: 'Завершенный' }}
+					selectedOption={null}
 				/>
 			</div>
 			<div className={styles.projectsContainer}>
