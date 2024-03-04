7e3e1caabc5099552b72c127fca98e2bd2331c12
DBKai <dbkai@ya.ru>
2024-03-04 23:38:07 +0500

    refactor(shared): moved styles to function

--- src/shared/ui/single-select/single-select.tsx
+++ src/shared/ui/single-select/single-select.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import React, { FC } from 'react';
-import Select, { components, OptionProps } from 'react-select';
+import Select, {
+	components,
+	ControlProps,
+	CSSObjectWithLabel,
+	GroupBase,
+	OptionProps,
+} from 'react-select';
 import { SingleSelectProps } from './type';
 import { Option } from '@/shared/types/option';
 import CheckIcon from '@/shared/assets/icons/check-icon.svg';
@@ -12,6 +18,86 @@ export const SingleSelect: FC<SingleSelectProps> = ({
 	options,
 	selectedOption,
 }) => {
+	const containerStyles = (base: CSSObjectWithLabel) => ({
+		...base,
+		width: '100%',
+	});
+
+	const placeholderStyles = (base: CSSObjectWithLabel) => ({
+		...base,
+		color: '#020617',
+	});
+
+	const optionStyles = (
+		base: CSSObjectWithLabel,
+		state: OptionProps<Option, boolean, GroupBase<Option>>
+	) => ({
+		...base,
+		minHeight: '44px',
+		padding: '0px 16px',
+		display: 'flex',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		fontFamily: 'Open Sans, sans-serif',
+		fontSize: '16px',
+		fontWeight: '400',
+		color: state.isSelected
+			? '#020617'
+			: state.isDisabled
+				? 'gray'
+				: state.isFocused
+					? '#020617'
+					: 'inherit',
+		backgroundColor: state.isFocused
+			? '#E8EEFF' // не выбранная опция
+			: state.isSelected
+				? 'transparent' // выбранная опция
+				: 'transparent', // обычное состояние
+	});
+
+	const controlStyles = (
+		base: CSSObjectWithLabel,
+		state: ControlProps<Option, boolean, GroupBase<Option>>
+	) => ({
+		...base,
+		width: '170px',
+		height: '48px',
+		padding: '8px 12px',
+		border: '1px solid #E2E8F0',
+		borderRadius: '1000px',
+		outline: 'none',
+		fontFamily: 'Open Sans, sans-serif',
+		fontSize: '16px',
+		fontWeight: '700',
+		color: state.isFocused ? '#020617' : '#E2E8F0',
+		// backgroundColor: state.isFocused ? '#E8EEFF' : 'white',
+		boxShadow: '0 0 0 0 transparent',
+		cursor: 'pointer',
+	});
+
+	const menuListStyles = (base: CSSObjectWithLabel) => ({
+		...base,
+		padding: '0px 0px',
+	});
+
+	const menuStyles = (base: CSSObjectWithLabel) => ({
+		...base,
+		width: '320px',
+		borderRadius: '12px',
+		border: '1px solid #E2E8F0',
+		boxShadow: '2px 2px 12px 0px rgba(62, 56, 56, 0.10)',
+	});
+
+	const singleValueStyles = (base: CSSObjectWithLabel) => ({
+		...base,
+		color: '#020617',
+	});
+
+	const indicatorSeparatorStyles = (base: CSSObjectWithLabel) => ({
+		...base,
+		display: 'none',
+	});
+
 	return (
 		<div>
 			<Select
@@ -29,69 +115,14 @@ export const SingleSelect: FC<SingleSelectProps> = ({
 				formatOptionLabel={() => caption}
 				onChange={(option) => console.log(option)}
 				styles={{
-					container: (base) => ({ ...base, width: '100%' }),
-					placeholder: (base) => ({
-						...base,
-						color: '#020617',
-					}),
-					option: (base, state) => ({
-						...base,
-						minHeight: '44px',
-						padding: '0px 16px',
-						display: 'flex',
-						justifyContent: 'space-between',
-						alignItems: 'center',
-						fontFamily: 'Open Sans, sans-serif',
-						fontSize: '16px',
-						fontWeight: '400',
-						color: state.isSelected
-							? '#020617'
-							: state.isDisabled
-								? 'gray'
-								: state.isFocused
-									? '#020617'
-									: 'inherit',
-						backgroundColor: state.isFocused
-							? '#E8EEFF' // не выбранная опция
-							: state.isSelected
-								? 'transparent' // выбранная опция
-								: 'transparent', // обычное состояние
-					}),
-					control: (base, state) => ({
-						...base,
-						width: '170px',
-						height: '48px',
-						padding: '8px 12px',
-						border: '1px solid #E2E8F0',
-						borderRadius: '1000px',
-						outline: 'none',
-						fontFamily: 'Open Sans, sans-serif',
-						fontSize: '16px',
-						fontWeight: '700',
-						color: state.isFocused ? '#020617' : '#E2E8F0',
-						// backgroundColor: state.isFocused ? '#E8EEFF' : 'white',
-
-						boxShadow: '0 0 0 0 transparent',
-						cursor: 'pointer',
-					}),
-					menuList: (base) => ({
-						...base,
-						padding: '0px 0px',
-					}),
-					menu: (base) => ({
-						...base,
-						width: '320px',
-						borderRadius: '12px',
-						border: '1px solid #E2E8F0',
-						boxShadow: '2px 2px 12px 0px rgba(62, 56, 56, 0.10)',
-					}),
-					singleValue: (base) => ({
-						...base,
-						color: '#020617',
-					}),
-					indicatorSeparator: () => ({
-						display: 'none',
-					}),
+					container: (base) => containerStyles(base),
+					placeholder: (base) => placeholderStyles(base),
+					option: (base, state) => optionStyles(base, state),
+					control: (base, state) => controlStyles(base, state),
+					menuList: (base) => menuListStyles(base),
+					menu: (base) => menuStyles(base),
+					singleValue: (base) => singleValueStyles(base),
+					indicatorSeparator: (base) => indicatorSeparatorStyles(base),
 				}}
 			/>
 		</div>
