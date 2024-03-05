6c3cca05dcc76984900fd7febfad9657e3c8daed
jsapro <77.3.77@mail.ru>
2024-03-05 21:42:11 +0300

    fix(app): errors preventing npm run build

--- src/pages/profile-user-page/ui/profile-user-page.tsx
+++ src/pages/profile-user-page/ui/profile-user-page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import React from 'react';
-import { useState } from 'react';
+// import { useState } from 'react';
 import { ProfileEditForm } from '@/entities/profile-edit-form';
-import { PopUp } from '@/shared/ui/pop-up/pop-up';
+// import { PopUp } from '@/shared/ui/pop-up/pop-up';
 
 export const ProfileUser = () => {
-	const [isPopup, setIsPopup] = useState(false);
-	const onClose = () => setIsPopup(false);
+	// const [isPopup, setIsPopup] = useState(false);
+	// const onClose = () => setIsPopup(false);
 
 	return (
 			<ProfileEditForm />
