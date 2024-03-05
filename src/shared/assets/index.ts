8237b3db620a65900cde40163d6ba23e0682b325
jsapro <77.3.77@mail.ru>
2024-03-05 20:38:38 +0300

    feat(entities): add ProjectFilter component

--- src/shared/assets/index.ts
+++ src/shared/assets/index.ts
@@ -1,4 +1,5 @@
 import ActivityIcon from '@/shared/assets/icons/activity-icon.svg';
 import CalendarIcon from '@/shared/assets/icons/calendar-icon.svg';
+import CloseIcon from './icons/xmark.svg';
 
-export { ActivityIcon, CalendarIcon };
+export { ActivityIcon, CalendarIcon, CloseIcon };
