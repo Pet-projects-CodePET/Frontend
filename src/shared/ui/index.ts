db2a991ff3327df5dff2a2160d7306c296c3abcb
jsapro <77.3.77@mail.ru>
2024-03-11 16:45:00 +0300

    feat(entities):
    change input to checkbox component

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -4,4 +4,5 @@ export { IconButton } from './icon-button/icon-button';
 export { Input } from './input/input';
 export { NavLinks } from './nav-links/nav-links';
 export { ProjectCard } from './project-card/project-card';
+export { Checkbox } from './checkbox/checkbox';
 export type { TagsProps, Tag } from './tags/types';
