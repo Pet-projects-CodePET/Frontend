47fa5848d4ce040604522006363a76c28a69b524
unknown <panterajuliapapina@yandex.ru>
2024-03-12 20:03:06 +0300

    feat(shared): added preview-profile in profile-edit-form

--- src/entities/profile-edit-form/ui/profile-edit-form.tsx
+++ src/entities/profile-edit-form/ui/profile-edit-form.tsx
@@ -1,15 +1,14 @@
 'use client';
 
-import React from 'react';
-
+import React, { useState } from 'react';
 import { MainButton } from '@/shared/ui';
 import { Input } from '@/shared/ui';
 import { Form } from '@/shared/ui';
 import { DatePickerRHF } from '@/shared/ui/date-picker-rhf/date-picker-rhf';
 import { useForm } from 'react-hook-form';
+import { PreviewProfile } from '@/shared/ui/preview-profile/preview-profile';
 import Edit from '@/shared/assets/icons/edit-icon.svg';
 import styles from './profile-edit-form.module.scss';
-import Link from 'next/link';
 
 export const ProfileEditForm = () => {
 	const { register, control } = useForm();
@@ -18,89 +17,115 @@ export const ProfileEditForm = () => {
 		console.log('Submit');
 	};
 
+	const [preview, setPreview] = useState(false);
+
+	const handlePreview = () => {
+		setPreview(true);
+	};
+
 	return (
-		<Form onSubmit={handleSubmit} className={styles.fields}>
-			<div className={styles.fields_photo}>
-				<div className={styles.fields_avatar}>
-					<div className={styles.fields_text}>A</div>
-				</div>
-				<button className={styles.fields_edit}>
-					<Edit />
-				</button>
-			</div>
-			<Input
-				label="nick_name"
-				labelName="Никнейм"
-				register={register}
-				description
-			/>
-			<Input
-				label="name"
-				labelName="Имя"
-				register={register}
-				description={true}
-				descrText="Укажите свое настоящее имя и фамилию"
-			/>
-			<Input
-				label="about"
-				labelName="О себе"
-				register={register}
-				description={true}
-				descrText="Не более 750 символов"
-			/>
-			<Input
-				label="portfolioLink"
-				labelName="Ссылка на портфолио"
-				register={register}
-				description={true}
-				descrText="Добавьте ссылку на любую платформу, где размещено ваше портфолио"
-			/>
-			<Input
-				label="contacts"
-				labelName="Контакты для связи"
-				register={register}
-				description={true}
-				descrText="Укажите контакты для связи, например: e-mail, telegram, телефон"
-			/>
-			<div className={styles.datePickerContainer}>
-				<p className={styles.datePickerTitle}>Дата рождения</p>
-				<DatePickerRHF control={control} name="birthDate" />
-			</div>
-			<div className={styles.fields_double}>
-				<Input
-					label="country"
-					labelName="Страна"
-					register={register}
-					description
-				/>
-				<Input label="city" labelName="Город" register={register} description />
-			</div>
-			<Input
-				label="speciality"
-				labelName="Специальность"
-				register={register}
-				description={true}
-				descrText="Выберите не более 2 специальностей"
-			/>
-			<Input
-				label="skills"
-				labelName="Навыки"
-				register={register}
-				description={true}
-				descrText="Выберите не более 15 навыков"
-			/>
-			<Input
-				label="qualLabel"
-				labelName="Уровень квалификации"
-				register={register}
-				description
-			/>
-			<div className={styles.fields_buttonsContainer}>
-				<MainButton variant={'primary'} width={'regular'}>
-					Сохранить
-				</MainButton>
-				<Link href="/">Как видят мой профиль другие</Link>
-			</div>
-		</Form>
+		<section>
+			{preview ? (
+				<PreviewProfile /*visible={preview} */ />
+			) : (
+				<Form onSubmit={handleSubmit} className={styles.fields}>
+					<div className={styles.fields_photo}>
+						<div className={styles.fields_avatar}>
+							<div className={styles.fields_text}>A</div>
+						</div>
+						<button className={styles.fields_edit}>
+							<Edit />
+						</button>
+					</div>
+					<Input
+						label="nick_name"
+						labelName="Никнейм"
+						register={register}
+						description
+					/>
+					<Input
+						label="name"
+						labelName="Имя"
+						register={register}
+						description={true}
+						descrText="Укажите свое настоящее имя и фамилию"
+					/>
+					<Input
+						label="about"
+						labelName="О себе"
+						register={register}
+						description={true}
+						descrText="Не более 750 символов"
+					/>
+					<Input
+						label="portfolioLink"
+						labelName="Ссылка на портфолио"
+						register={register}
+						description={true}
+						descrText="Добавьте ссылку на любую платформу, где размещено ваше портфолио"
+					/>
+					<Input
+						label="contacts"
+						labelName="Контакты для связи"
+						register={register}
+						description={true}
+						descrText="Укажите контакты для связи, например: e-mail, telegram, телефон"
+					/>
+					<div className={styles.datePickerContainer}>
+						<p className={styles.datePickerTitle}>Дата рождения</p>
+						<DatePickerRHF control={control} name="birthDate" />
+					</div>
+					<div className={styles.fields_double}>
+						<Input
+							label="country"
+							labelName="Страна"
+							register={register}
+							description
+						/>
+						<Input
+							label="city"
+							labelName="Город"
+							register={register}
+							description
+						/>
+					</div>
+					<Input
+						label="speciality"
+						labelName="Специальность"
+						register={register}
+						description={true}
+						descrText="Выберите не более 2 специальностей"
+					/>
+					<Input
+						label="skills"
+						labelName="Навыки"
+						register={register}
+						description={true}
+						descrText="Выберите не более 15 навыков"
+					/>
+					<Input
+						label="qualLabel"
+						labelName="Уровень квалификации"
+						register={register}
+						description
+					/>
+					<div className={styles.fields_buttonsContainer}>
+						<MainButton variant={'primary'} width={'regular'}>
+							Сохранить
+						</MainButton>
+						<button
+							type="button"
+							className={styles.button__preview}
+							onClick={handlePreview}>
+							{'Как видят мой профиль другие'}
+						</button>
+					</div>
+				</Form>
+			)}
+
+			
+		</section>
+
+	
 	);
 };
---
+++ src/shared/ui/preview-profile/preview-profile.tsx
@@ -0,0 +1,97 @@
+'use client';
+
+import React, { useState } from 'react';
+import { ProfileEditForm } from '@/entities/profile-edit-form';
+import Link from 'next/link';
+import styles from './preview-profile.module.scss';
+
+export const PreviewProfile = () => {
+	const [visible, setVisible] = useState(false);
+
+	const handleClick = () => {
+		setVisible(true);
+	};
+
+	return (
+		<section className={styles.preview}>
+			{!visible ? (
+				<>
+					<div className={styles.previewWrapper}>
+						<div className={styles.previewFoto}>
+							<div className={styles.previewFoto__avatar} />
+							<div className={styles.previewFoto__info}>
+								<h1 className={styles.previewFoto__infoName}>
+									Длиннофамильная Екатерина
+								</h1>
+								<p className={styles.previewFoto__infoTelegram}>@nickname</p>
+								<p className={styles.previewFoto__infoRole}>
+									UX/UI designer / UX/UI дизайнер, Middle
+								</p>
+								<p className={styles.previewFoto__infoStatus}>
+									{' '}
+									готов(а) к участию в проекте
+								</p>
+							</div>
+						</div>
+						<div className={styles.previewItem}>
+							<h2 className={styles.previewItem__title}>Навыки</h2>
+							<p className={styles.previewItem__subtitle}>
+								Python, JavaScript, C#, C++{' '}
+							</p>
+						</div>
+
+						<div className={styles.previewItem}>
+							<h2 className={styles.previewItem__title}>О себе</h2>
+							<p className={styles.previewItem__subtitle}>
+								Не люблю заполнять описание Lorem ipsum dolor sit amet,
+								consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+								labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+								nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+								commodo consequat. Duis aute irure dolor in reprehenderit in
+								voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+								Excepteur sint occaecat cupidatat non proident, sunt in culpa
+								qui officia deserunt mollit anim id est laborum, но приходится.
+							</p>
+
+							<div className={styles.previewItem}>
+								<h2 className={styles.previewItem__title}>
+									Ссылка на портфолио
+								</h2>
+								<Link
+									href="#"
+									target="_blank"
+									className={styles.previewItem__subtitle}>
+									behance
+								</Link>
+							</div>
+
+							<div className={styles.previewItem}>
+								<h2 className={styles.previewItem__title}>
+									Контакты для связи
+								</h2>
+							</div>
+							<div className={styles.previewItem}>
+								<h2 className={styles.previewItem__title}>Дата рождения</h2>
+								<p className={styles.previewItem__subtitle}>01.02.1987</p>
+							</div>
+
+							<div className={styles.previewItem}>
+								<h2 className={styles.previewItem__title}>Регион</h2>
+								<p className={styles.previewItem__subtitle}>Россия, Омск</p>
+							</div>
+						</div>
+
+						<button
+							className={styles.previewButton}
+							type="button"
+							onClick={handleClick}>
+							{'На страницу редактирования'}
+						</button>
+					</div>
+				</>
+			) : (
+				<ProfileEditForm />
+			)}
+		</section>
+	);
+};
