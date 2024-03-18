867833056e4d28d73ca7c30b537342321d608a0c
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-03-18 13:30:53 +0300

    fix(app): fix email validation, comment reduxtoolkit actions

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,11 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import userReducer from '@/store/reducers/UserSlice';
-import projectReducer from '@/store/reducers/ProjectSlice';
+// import userReducer from '@/store/reducers/UserSlice';
+// import projectReducer from '@/store/reducers/ProjectSlice';
 import { userApi } from '@/services/UserService';
 
 const rootReducer = combineReducers({
-	userReducer,
-	projectReducer,
+	// userReducer,
+	// projectReducer,
 	[userApi.reducerPath]: userApi.reducer,
 });
 
