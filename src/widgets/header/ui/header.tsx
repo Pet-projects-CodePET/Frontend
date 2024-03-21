b7b32b0a6a1fe0c3af379101df8aa7dc00fa1a68
unknown <panterajuliapapina@yandex.ru>
2024-03-21 20:28:15 +0300

    fix(widgets): added icon login in header for new desing, changed styles, size

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -6,11 +6,11 @@ import Link from 'next/link';
 import { MainButton } from '@/shared/ui';
 import { NavBar } from '@/entities/nav-bar';
 import { navBarLinksArray } from '@/shared/constants';
-import IconLogin from '@/shared/assets/icons/icon-button-login.svg';
 import { MenuProfile } from '@/entities/menu-profile';
 import LogoIcon from '@/shared/assets/images/logo-header.svg';
 import MenuBurger from '@/shared/assets/icons/dots-vertical.svg';
 import IconPlus from '@/shared/assets/icons/plus-large.svg';
+import IconLogin from '@/shared/assets/icons/icon-come-in.svg';
 import styles from './header.module.scss';
 
 export const Header = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
@@ -19,12 +19,13 @@ export const Header = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
 	return (
 		<div className={styles.header}>
 			<div className={styles.header__container}>
-				<div className={styles.header__wrapper}>
+				<div
+					className={styles.header__wrapper}>
 					<Link href="/" className={styles.header__link}>
 						<LogoIcon alt="logo" className={styles.header__logo} />
 					</Link>
 					<NavBar navBarLinksArray={navBarLinksArray} />
-					{isLoggedIn ? (
+					{isLoggedIn && (
 						<div className={styles.header__buttonProject}>
 							<MainButton
 								variant="inverse"
@@ -34,27 +35,19 @@ export const Header = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
 								Создать проект
 							</MainButton>
 						</div>
-					) : (
-						<div className={styles.header__buttonLogin}>
-							<div className={styles.header__button}>
-								<MainButton
-									variant="inverse"
-									width="max"
-									onClick={() => router.push('login')}>
-									Войти
-								</MainButton>
-							</div>
-							<Link href="/login" className={styles.header__iconLogin}>
-								<IconLogin className={styles.header__iconLogin} />
-							</Link>
-						</div>
 					)}
 
 					<button className={styles.header__buttonBurger}>
 						<MenuBurger className={styles.header__buttonBurgerIcon} />
 					</button>
 				</div>
-				{isLoggedIn && <MenuProfile />}
+				{isLoggedIn ? (
+					<MenuProfile />
+				) : (
+					<Link href="/login" className={styles.header__linkLogin}>
+						<IconLogin className={styles.header__iconLogin} />
+					</Link>
+				)}
 			</div>
 		</div>
 	);
