a0f9248f520414e896d70e3f782133eaf420d77b
unknown <panterajuliapapina@yandex.ru>
2024-03-25 12:17:09 +0300

    feat(pages): added specialists page blocks

--- src/pages/specialists-page/ui/specialists-page.tsx
+++ src/pages/specialists-page/ui/specialists-page.tsx
@@ -1,5 +1,29 @@
+'use client';
 import React from 'react';
+import { SpecialistCard } from '@/widgets/specialist-card';
+import { Header } from '@/widgets/header';
+import { Footer } from '@/widgets/footer';
+import { InputSearch } from '@/shared/ui/input-search/input-search';
+import styles from './specialists-page.module.scss';
 
 export const Specialists = () => {
-	return <div>Специалисты</div>;
+	return (
+		<>
+			<section className={styles.specialists}>
+				<div className={styles.specialists__wrapper}>
+					<Header isLoggedIn />
+					<div className={styles.specialists__container}>
+						<h1 className={styles.specialists__title}>Специалисты</h1>
+						<div className={styles.specialists__inputSearch}>
+							<InputSearch search={() => {}} onChange={() => {}} />
+						</div>
+					</div>
+					<div>Селекторы </div>
+				</div>
+				<SpecialistCard />
+				<div>ПАГИНАЦИЯ</div>
+			</section>
+			<Footer />
+		</>
+	);
 };
