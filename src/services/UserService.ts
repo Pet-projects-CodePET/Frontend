ddb7e05cc5e83e06a9a9fca3d06b41f5b12245ad
Ivan Vorobyov <ivanvorobyov1992@yandex.ru>
2024-04-01 23:42:25 +0300

    refactor(app): update UserService

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,14 +1,15 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { IUser } from '@/services/models/IUser';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { IUser } from '@/services/models/IUser' // const BASE_DEV_URL = process.env.NEXT_PUBLIC_BASE_DEV_URL;
 
-const BASE_DEV_URL = process.env.NEXT_PUBLIC_BASE_DEV_URL;
-const BASE_TEST_URL = process.env.NEXT_PUBLIC_BASE_TEST_URL;
+// const BASE_DEV_URL = process.env.NEXT_PUBLIC_BASE_DEV_URL;
+// const BASE_TEST_URL = process.env.NEXT_PUBLIC_BASE_TEST_URL;
+const BASE_URL = process.env.DOMAIN_NAME;
 
 export const userApi = createApi({
 	reducerPath: 'userApi',
 	baseQuery: fetchBaseQuery({
-		baseUrl:
-			process.env.NODE_ENV === 'production' ? BASE_TEST_URL : BASE_DEV_URL,
+		baseUrl: `https://${BASE_URL}/api/v1`,
+		// process.env.NODE_ENV === 'production' ? BASE_TEST_URL : BASE_DEV_URL,
 		prepareHeaders: async (headers) => {
 			const accessToken = localStorage.getItem('token');
 			if (accessToken) {
