f7a78cccd4eef17af3350c234217c55d0500a980
unknown <panterajuliapapina@yandex.ru>
2024-04-22 17:32:24 +0300

    feat(pages): removed counter from the markup profile-create-project-page

--- src/pages/profile-create-project-page/ui/profile-create-project-page.tsx
+++ src/pages/profile-create-project-page/ui/profile-create-project-page.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Counter } from '@/shared/ui';
 
 export const ProfileCreateProject = () => {
 	return (
 		<section>
 			<div>Форма создания проекта</div>
-			<Counter disabled={false}/>
 		</section>
 	);
 };
