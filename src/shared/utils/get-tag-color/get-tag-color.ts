75e937253b013b7377a10381461464569bd2cc3d
unknown <panterajuliapapina@yandex.ru>
2024-05-13 10:10:04 +0300

    fix(widgets): return tags for the layout project-card-full

--- src/shared/utils/get-tag-color/get-tag-color.ts
+++ src/shared/utils/get-tag-color/get-tag-color.ts
@@ -16,3 +16,23 @@ export const getColorTag = (specialty: string) => {
 			return '';
 	}
 };
+
+// Для верстки компонента project-card-full на странице "Проекты"
+export const getClassNameforTag = (profession: string) => {
+	switch (profession) {
+		case 'UX/UI':
+			return 'profession_type_colorSulu';
+		case 'Manual QA':
+			return 'profession_type_colorRajah';
+		case 'PM':
+			return 'profession_type_colorMalibu';
+		case 'System Analyst':
+			return 'profession_type_colorPrelude';
+		case 'Front-end':
+			return 'profession_type_colorFroly';
+		case 'DevOps':
+			return 'profession_type_colorAnakiwa';
+		default:
+			return '';
+	}
+};
