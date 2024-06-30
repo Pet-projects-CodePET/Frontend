aac8b7da23d36a3005e0c2ed7b2706d12aa5060e
makc-anisimov <makc.anisimov@gmail.com>
2024-06-30 22:49:37 +0300

    style(entities): add index file  for entities list

---
+++ src/entities/contact-card/ui/contact-card.tsx
@@ -0,0 +1,12 @@
+
+import React, { FC } from 'react';
+import { ContactCardProps } from './types';
+
+export const ContactCard: FC<ContactCardProps> = () => {
+
+  return (
+    <>
+    </>
+  )
+}
+
---
+++ src/utils/constants.ts
@@ -0,0 +1,52 @@
+export const EMPLOYMENT = [
+	{
+		id: 1,
+		name: '10 часов в неделю',
+	},
+	{
+		id: 2,
+		name: '20 часов в неделю',
+	},
+	{
+		id: 3,
+		name: '30 часов в неделю',
+	},
+	{
+		id: 4,
+		name: '40 часов в неделю',
+	},
+];
+
+export const LEVEL = [
+	{
+		label: 'Junior',
+		value: 'Junior',
+	},
+	{
+		label: 'Middle',
+		value: 'Middle',
+	},
+	{
+		label: 'Senior',
+		value: 'Senior',
+	},
+	{
+		label: 'Lead',
+		value: 'Lead',
+	},
+];
+
+export const CONTACTS = [
+	{
+		label: 'telegram',
+		value: 'telegram',
+	},
+	{
+		label: 'email',
+		value: 'email',
+	},
+	{
+		label: 'phone',
+		value: 'phone',
+	},
+];
