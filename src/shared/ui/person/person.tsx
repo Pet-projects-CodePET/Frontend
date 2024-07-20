052ac87fda7cc7db29f917b7031bd67e5a6c0feb
unknown <panterajuliapapina@yandex.ru>
2024-07-20 12:05:52 +0300

    fix(shared): added check the link in person if there is no id

--- src/shared/ui/person/person.tsx
+++ src/shared/ui/person/person.tsx
@@ -1,27 +1,24 @@
 'use client';
-import React, {FC} from 'react';
+import React, { FC } from 'react';
 import { PersonIcon } from '@/shared/assets';
 import Link from 'next/link';
 import { PersonType } from './type';
 import styles from './person.module.scss';
 
-export const Person: FC<PersonType> = ({
-	title,
-	avatar,
-	color,
-	id,
-}) => {
+export const Person: FC<PersonType> = ({ title, avatar, color, id }) => {
 	const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 	return (
 		<div className={styles.container}>
-			<Link className={styles.link} href={`/specialists/${id}`}>
-			{avatar ? (
-				<div
-					className={styles.iconLink}
-					style={{ backgroundImage: `url('https://${BASE_URL}${avatar}')` }}></div>
-			) : (
-				<PersonIcon className={styles.icon} />
-			)}
+			<Link className={styles.link} href={id ? `/specialists/${id}` : ''}>
+				{avatar ? (
+					<div
+						className={styles.iconLink}
+						style={{
+							backgroundImage: `url('https://${BASE_URL}${avatar}')`,
+						}}></div>
+				) : (
+					<PersonIcon className={styles.icon} />
+				)}
 			</Link>
 
 			<p
--- src/widgets/project-card-detailed/ui/project-card-detailed.tsx
+++ src/widgets/project-card-detailed/ui/project-card-detailed.tsx
@@ -24,11 +24,9 @@ export const ProjectCardDetailed: FC<ProjectCardDetailType> = ({
 	ended,
 	status,
 	project_specialists,
-	
 }) => {
 	const startDate = getStartDate(started);
 	const endDate = getEndDate(ended);
-	
 
 	const skillsTeam = [
 		{ id: 1, name: 'React' },
@@ -110,14 +108,21 @@ export const ProjectCardDetailed: FC<ProjectCardDetailType> = ({
 					</div>
 					<div className={styles.subtitleWrapper}>
 						<h3 className={styles.subtitle}>Ссылка на проект</h3>
-						<Link  href={`${link}`} target="_blank" className={styles.descriptionLink}>
-							 {link} 
+						<Link
+							href={`${link}`}
+							target="_blank"
+							className={styles.descriptionLink}>
+							{link}
 						</Link>
 					</div>
 					<div className={styles.subtitleWrapper}>
 						<h3 className={styles.subtitle}>Организатор</h3>
 						<div className={styles.personWrapper}>
-							<Person title={owner?.name || owner?.username} avatar={owner?.avatar} id={owner?.id}/>
+							<Person
+								title={owner?.name || owner?.username}
+								avatar={owner?.avatar}
+								id={owner?.id}
+							/>
 						</div>
 					</div>
 					<div className={styles.subtitleWrapper}>
