6458838e16c160448a0e1f2e3eb4c68e4da58cec
unknown <panterajuliapapina@yandex.ru>
2024-09-08 22:03:29 +0300

    refactor(shared): change styles login layout, form login

--- src/shared/layouts/login-layout/login-layout.tsx
+++ src/shared/layouts/login-layout/login-layout.tsx
@@ -5,14 +5,15 @@ import Link from 'next/link';
 
 export const LoginLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<div className={styles.container}>
-			<div className={styles.logo}>
-				<Link href="/" className={styles.logo__link}>
-					<LogoIcon alt="logo" className={styles.logo__icon} />
-				</Link>
+		<div className={styles.login}>
+			<div className={styles.children}>
+				{children}
+				<div className={styles.logo}>
+					<Link href="/" className={styles.logo__link}>
+						<LogoIcon alt="logo" className={styles.logo__icon} />
+					</Link>
+				</div>
 			</div>
-
-			{children}
 		</div>
 	);
 };
